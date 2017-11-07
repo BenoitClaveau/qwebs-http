78b9922ce377e55b4579fb9d4afd0146c97b55c7
Benoît Claveau <benoit.claveau@izzili.com>
2017-11-07 12:42:47 +0100

    Unit test

---
+++ tests/services/reply.spec.js
@@ -0,0 +1,74 @@
+/*!
+ * qwebs
+ * Copyright(c) 2016 Benoît Claveau <benoit.claveau@gmail.com>
+ * MIT Licensed
+ */
+"use strict";
+
+const expect = require("expect.js");
+const Qwebs = require("qwebs");
+const { FromArray } = require("qwebs");
+const http = require("http");
+const request = require('request');
+
+require("process").on('unhandledRejection', (reason, p) => {
+    console.error('Unhandled Rejection at:', p, 'reason:', reason);
+});
+
+describe("reponse", () => {
+
+    // it("JSON string", () => {
+    //     let server = http.createServer((request, response) => {
+    //         response.writeHead(200, {'Content-Type': 'application/json'});
+    //         response.end('{ "id": 3 }');
+    //     }).listen(1339, () => {
+    //         request({ method: 'GET', uri: 'http://localhost:1339', json: true }, (error, response, body) => {
+    //             server.close()
+    //             expect(error).to.be(null);
+    //             expect(body.id).to.be(3);
+    //         });
+    //     });
+    // });
+
+    it("JSON stream", async () => {
+        let qwebs = new Qwebs({ dirname: __dirname, config: {}});
+        const json = await qwebs.resolve("$json-stream");
+
+        let server = http.createServer((request, response) => {
+            const stream = new FromArray([{ id: 1}, { id: 2}, { id: 3}, { id: 4}]);
+            stream.pipe(json.stringify).pipe(response);
+        }).listen(1340, () => {
+            request({ method: 'GET', uri: 'http://localhost:1340', json: true }, (error, response, body) => {
+                server.close()
+                expect(error).to.be(null);
+                expect(body.length).to.be(4);
+            });
+        });
+    });
+
+    it("Reply stream", async () => {
+        try {
+        let qwebs = new Qwebs({ dirname: __dirname, config: {}});
+        qwebs.inject("Reply", "../../lib/services/reply", { instanciate: false });
+		
+        const json = await qwebs.resolve("$json-stream");
+        const Reply = await qwebs.resolve("Reply");
+
+        let server = http.createServer((request, response) => {
+            const stream = new FromArray([{ id: 1}, { id: 2}, { id: 3}, { id: 4}]);
+            //stream.pipe(json.stringify).pipe(response);
+            stream.pipe(new Reply(request, response));
+        }).listen(1341, () => {
+            request({ method: 'GET', uri: 'http://localhost:1341', json: true }, (error, response, body) => {
+                server.close()
+                expect(error).to.be(null);
+                expect(body.length).to.be(4);
+            });
+        });
+        }
+        catch (error) {
+            throw error;
+        }
+    });
+
+});

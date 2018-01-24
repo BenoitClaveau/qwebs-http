cff373fac49366cbb6dcdf6e26a5d4192632b105
Benoît Claveau <benoit.claveau@izzili.com>
2018-01-24 16:13:57 +0100

    Http2

--- tests/http-router.http2.spec.js
+++ tests/http-router.http2.spec.js
@@ -8,6 +8,7 @@ const Qwebs = require("qwebs");
 const process = require("process");
 const { inspect } = require("util");
 const http2 = require('http2');
+const fs = require('fs');
 
 process.on("unhandledRejection", (reason, p) => {
     console.error("Unhandled Rejection at:", p, "reason:", inspect(reason));
@@ -16,52 +17,70 @@ process.on("unhandledRejection", (reason, p) => {
 let qwebs;
 beforeEach(() => qwebs = new Qwebs({ dirname: __dirname, config: { 
     http2: { 
-        port: 3000, 
+        port: 8443, 
         cert: `${__dirname}/certificates/certificate.pem`,
         key: `${__dirname}/certificates/private-key.pem`,
     }
 }}));
 afterEach(async () => await qwebs.unload());
 
+const timeout = ms => new Promise(res => setTimeout(res, ms))
+
 describe("http-router", () => {
+
+    xit("single route", async () => {
+        const client = http2.connect('https://stackoverflow.com');
+
+        const req = client.request();
+        req.setEncoding('utf8');
+
+        req.on('response', (headers, flags) => {
+            console.log(headers);
+        });
+
+        let data = '';
+        req.on('data', (d) => data += d);
+        req.on('end', () => {
+            console.log(data);
+            client.destroy();
+        })
+        req.end();
+    }).timeout(60000);
 
     it("single route", async () => {
         qwebs.inject("$http", "../index");
         qwebs.inject("$info", "./services/info");
         await qwebs.load();
+        
+        await timeout(600000)
+
         const http = await qwebs.resolve("$http");
         http.get("/whoiam", "$info", "whoiam");
         http.get("/helloworld", "$info", "helloworld");
-        //const client = await qwebs.resolve("$client");
-        const client = http2.connect('https://localhost:3000');
-        client.on('stream', (pushedStream, requestHeaders) => {
-            pushedStream.on('push', (responseHeaders) => {
-                // process response headers
-            });
-            pushedStream.on('data', (chunk) => { /* handle pushed data */ });
+        
+        const client = http2.connect('https://localhost:8443',  {
+           ca: fs.readFileSync(`${__dirname}/certificates/certificate.pem`)
         });
-          
-        const req = client.request({ 
-            ':path': '/whoiam' 
+        //const client = http2.connect('http://localhost:3000');
+        const req = client.request({ ':method': 'GET', ':path': '/' });
+        req.on('response', (headers, flags) => {
+            for (const name in headers) {
+                console.log(`${name}: ${headers[name]}`);
+            }
         });
-        req.on('response', (headers) => {
-            console.log(headers[http2.constants.HTTP2_HEADER_STATUS]);
-        });
-        let data = '';
+
         req.setEncoding('utf8');
-        req.on('data', (chunk) => data += chunk);
+        let data = '';
+        req.on('data', (chunk) => { data += chunk; });
         req.on('end', () => {
-            console.log(`The server says: ${data}`);
+            console.log(`\n${data}`);
             client.close();
         });
-        //req.end('Jane');
+        req.end();
 
-        const timeout = ms => new Promise(res => setTimeout(res, ms))
 
-        await timeout(10000)
-        console.log("DONE")
-        //const res = await client.get("https://localhost:3000/whoiam");
-        //expect(res.body).to.be("I'm Info service.");
-    }).timeout(60000);
+    }).timeout(600000);
+
+    
 
 });
